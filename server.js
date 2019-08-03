a4dfbbbfd79ade88017f7c02b8cb086af488de4a
nikhiljohnbejoy <nikhiljohnbejoy@yahoo.co.in>
2019-08-03 12:32:09 +0530

    MongoDB

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ var bodyParser=require('body-parser');
 var app=express();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
+var mongoose=require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+var dbUrl='mongodb+srv://user:user@cluster0-kdygx.mongodb.net/test?retryWrites=true&w=majority';
+
+var Message=mongoose.model('Message',{
+    name:String,
+    message:String
+})
+
 var messages =
     [
         {name:"Tim",message:"this is Tim"},
@@ -16,16 +25,30 @@ var messages =
     ]
 
 app.get('/messages',(req,res)=>{
-    res.send(messages);
+   // res.send(messages);
+   Message.find({},(err,data)=>{
+        res.send(data);
+   })
 });
 app.post('/messages',(req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body);
-    res.sendStatus(200);
+    var message=new Message(req.body);
+    message.save((err)=>{
+        if(err)
+            sendStatus(500);
+        //messages.push(req.body);
+        io.emit('message',req.body);
+        res.sendStatus(200);
+    })
+    
 });
 io.on('connection',(socket)=>{
     console.log('user connected');
 })
+
+mongoose.connect(dbUrl,{ useNewUrlParser: true},(err)=>{
+    console.log('mongo db connection ',err);
+})
+
 var server=http.listen(3000,()=>{
     console.log('listening on port ', server.address().port);
 });
